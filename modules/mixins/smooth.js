eecaafd08adac19ad3f61a173ea40a1f51d9e366
Javier Rojas <javierrojas.ads@gmail.com>
2021-06-07 14:34:11 -0400

    New easings Expo, Back, Elastic: In Out & InOut

--- modules/mixins/smooth.js
+++ modules/mixins/smooth.js
@@ -38,5 +38,24 @@ export default {
   // decelerating to zero velocity
   easeOutQuint: (x) =>  1 + (--x) * x * x * x * x,
   // acceleration until halfway, then deceleration 
-  easeInOutQuint: (x) =>   x < .5 ? 16 * x * x * x * x * x : 1 + 16 * (--x) * x * x * x * x
+  easeInOutQuint: (x) =>   x < .5 ? 16 * x * x * x * x * x : 1 + 16 * (--x) * x * x * x * x,
+  // deceleration until 25% then accelerates faster
+  easeInExpo: (x) => x === 0 ? 0 : pow(2, 10 * x - 10),
+  // accelerates until 25% then deceleration
+  easeOutExpo: (x) => x === 1 ? 1 : 1 - pow(2, -10 * x),
+  // deceleration at halfway, but fast at beginning and end
+  easeInOutExpo: (x) => x === 0 ? 0 : x === 1 ? 1 : x < 0.5 ? pow(2, 20 * x - 10) / 2 : (2 - pow(2, -20 * x + 10)) / 2,
+  // goes back then accelerate to end and stop
+  easeInBack: (x) => 2.70158 * x * x * x - 1.70158 * x * x,
+  // goes a little beyond at end
+  easeOutBack: (x) => 1 + 2.70158 * pow(x - 1, 3) + 1.70158 * pow(x - 1, 2),
+  // goes back at beginning and goes beyond at end
+  easeInOutBack: (x) => x < 0.5 ? (pow(2 * x, 2) * ((1.70158 + 1) * 2 * x - 2.5949095)) / 2 : (pow(2 * x - 2, 2) * ((2.5949095 + 1) * (x * 2 - 2) + 2.5949095) + 2) / 2,
+  // very bouncy at end
+  easeInElastic: (x) => x === 0 ? 0 : x === 1 ? 1 : -pow(2, 10 * x - 10) * sin((x * 10 - 10.75) * ((2 * Math.PI) / 3)),
+  // very bouncy at beginning 
+  easeOutElastic: (x) => x === 0 ? 0 : x === 1 ? 1 : pow(2, -10 * x) * sin((x * 10 - 0.75) * ((2 * Math.PI) / 3)) + 1,
+  // very bounce at beginning and at end
+  easeInOutElastic: (x) => x === 0 ? 0 : x === 1 ? 1 : x < 0.5 ? -(pow(2, 20 * x - 10) * sin((20 * x - 11.125) * c5)) / 2  : (pow(2, -20 * x + 10) * sin((20 * x - 11.125) * c5)) / 2 + 1
+
 };
